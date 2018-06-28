668f089f63c1d2383cb6e5334d903a2f2d5359d1
Bartosz Jurga <Bartosz.Jurga@ubs.com>
2018-06-28 17:08:30 +0200

    jakies konflikty zjebane

--- src/main/java/pl/bjur/shooter/club/Club.java
+++ src/main/java/pl/bjur/shooter/club/Club.java
@@ -6,7 +6,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import pl.bjur.shooter.address.Address;
 import pl.bjur.shooter.commons.EntityId;
-<<<<<<< HEAD
 import pl.bjur.shooter.user.User;
 
 import javax.persistence.Entity;
@@ -15,12 +14,6 @@ import javax.persistence.OneToOne;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.util.Set;
-=======
-
-import javax.persistence.Entity;
-import javax.persistence.OneToOne;
->>>>>>> 3ad0c7aa2ec0cba142a022b4d6fa58ea1d12869b
-
 
 @Entity
 @Getter
@@ -29,15 +22,11 @@ import javax.persistence.OneToOne;
 @NoArgsConstructor
 public class Club extends EntityId {
 
-<<<<<<< HEAD
     @NotBlank
-=======
->>>>>>> 3ad0c7aa2ec0cba142a022b4d6fa58ea1d12869b
     private String displayName;
 
     @OneToOne
     private Address address;
-<<<<<<< HEAD
 
     @OneToOne
     @NotBlank
@@ -54,6 +43,4 @@ public class Club extends EntityId {
 
     @NotNull
     private Boolean isMemberRegistrationOpen;
-=======
->>>>>>> 3ad0c7aa2ec0cba142a022b4d6fa58ea1d12869b
 }
--- src/main/java/pl/bjur/shooter/club/ClubRepository.java
+++ src/main/java/pl/bjur/shooter/club/ClubRepository.java
@@ -1,9 +1,5 @@
 package pl.bjur.shooter.club;
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 3ad0c7aa2ec0cba142a022b4d6fa58ea1d12869b
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClubRepository extends JpaRepository<Club, Long> {
