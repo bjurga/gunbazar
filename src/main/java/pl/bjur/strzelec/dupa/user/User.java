e871e2b4c4640bc86c5f5e58f5ac9c87ab939892
Bartosz Jurga <Bartosz.Jurga@ubs.com>
2018-06-28 14:25:47 +0200

    Added User class Rearranged packages

---
+++ src/main/java/pl/bjur/strzelec/dupa/user/User.java
@@ -0,0 +1,26 @@
+package pl.bjur.strzelec.dupa.user;
+
+import lombok.*;
+import pl.bjur.strzelec.dupa.commons.EntityWithId;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Getter @Setter
+@EqualsAndHashCode(callSuper = true)
+@AllArgsConstructor
+@NoArgsConstructor
+public class User extends EntityWithId{
+
+    @Column(unique = true)
+    @NotBlank private String displayName;
+
+    @Email @Column(unique = true)
+    @NotBlank private String email;
+
+    @NotNull private String password;
+}
