25354f6793516021f1cac48cb76819c958e576cd
Bartosz Jurga <Bartosz.Jurga@ubs.com>
2018-06-28 15:05:28 +0200

    removed @repository

--- src/main/java/pl/bjur/shooter/club/ClubRepository.java
+++ src/main/java/pl/bjur/shooter/club/ClubRepository.java
@@ -2,8 +2,6 @@ package pl.bjur.shooter.club;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface ClubRepository extends JpaRepository<Club, Long> {
 }
